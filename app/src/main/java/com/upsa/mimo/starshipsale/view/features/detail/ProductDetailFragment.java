17ff45fa679262ef733d4567a4b6f55cb560ba7a
Tylos <grtylos@gmail.com>
2016-05-01 14:28:02 +0200

    Style product detail

--- app/src/main/java/com/upsa/mimo/starshipsale/view/features/detail/ProductDetailFragment.java
+++ app/src/main/java/com/upsa/mimo/starshipsale/view/features/detail/ProductDetailFragment.java
@@ -1,16 +1,7 @@
 package com.upsa.mimo.starshipsale.view.features.detail;
 
-import com.squareup.picasso.Picasso;
-import com.upsa.mimo.starshipsale.BuildConfig;
-import com.upsa.mimo.starshipsale.R;
-import com.upsa.mimo.starshipsale.api.product.ApiProductRepository;
-import com.upsa.mimo.starshipsale.domain.entities.Product;
-
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Fragment;
-import android.graphics.Point;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,10 +9,15 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.upsa.mimo.starshipsale.BuildConfig;
+import com.upsa.mimo.starshipsale.R;
+import com.upsa.mimo.starshipsale.api.product.ApiProductRepository;
+import com.upsa.mimo.starshipsale.domain.entities.Product;
 
 import java.io.IOException;
 
@@ -83,152 +79,6 @@ public class ProductDetailFragment extends Fragment {
         mCrew = (TextView) view.findViewById(R.id.product_crew);
         mCargo = (TextView) view.findViewById(R.id.product_cargo);
         mHyperdriveRating = (TextView) view.findViewById(R.id.product_hyperdrive_rating);
-
-
-        //testOpenReveal();
-        //testCloseReveal();
-        mExtendedFab.setVisibility(View.GONE);
-        mFab.setVisibility(View.VISIBLE);
-
-        mFab.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-            public boolean mSet;
-
-            @Override
-            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                if (!mSet) {
-                    mSet = true;
-                    mFab.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            final int[] fabXY = new int[2];
-                            mFab.getLocationOnScreen(fabXY);
-
-                            final int[] ctaXY = new int[2];
-
-                            final Point extendedFabCenter = center(mExtendedFab);
-                            final Point fabCenter = center(mFab);
-
-                            mExtendedFab.getLocationOnScreen(ctaXY);
-                            mFab.animate()
-                                    .translationX(-(fabCenter.x - extendedFabCenter.x))
-                                    .translationY(-(fabCenter.y - extendedFabCenter.y));
-
-                        }
-                    }, 1000);
-                }
-            }
-        });
-
-
-
-        /*final CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) mFab.getLayoutParams();
-        layoutParams.setBehavior(
-        new CoordinatorLayout.Behavior<FloatingActionButton>() {
-
-            @Override
-            public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
-                return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
-            }
-
-            @Override
-            public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-                super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-
-
-                NestedScrollView nestedScrollView = (NestedScrollView) target;
-                final int scrollY = nestedScrollView.getScrollY();
-                final int contentHeight = nestedScrollView.getChildAt(0).getHeight();
-
-
-                if (reachedEnd(nestedScrollView, scrollY, contentHeight) && mExtendedFab.getVisibility() == View.GONE) {
-                    animateToBottomBar(child, mExtendedFab);
-                } else {
-                    animateToFab(child, mExtendedFab);
-                }
-            }
-
-            private boolean reachedEnd(NestedScrollView nestedScrollView, int scrollY, int contentHeight) {
-                return scrollY == contentHeight - nestedScrollView.getHeight();
-            }
-
-            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-            private void animateToBottomBar(final FloatingActionButton fab, final View bottomFabBar) {
-//                final int[] fabXY = new int[2];
-//                fab.getLocationOnScreen(fabXY);
-//
-//                final int[] ctaXY = new int[2];
-//                cta.getLocationOnScreen(ctaXY);
-//
-//                final Animator circularReveal = ViewAnimationUtils.createCircularReveal(
-//                        cta,
-//                        cta.getWidth() / 2,
-//                        cta.getHeight() / 2,
-//                        fab.getWidth() / 2,
-//                        cta.getWidth());
-//
-//                circularReveal
-//                        .addListener(new AnimatorListenerAdapter() {
-//
-//                            @Override
-//                            public void onAnimationStart(Animator animation) {
-//                                cta.setVisibility(View.VISIBLE);
-//                            }
-//                        });
-//                circularReveal.start();
-
-
-//                fab.animate()
-//                        .translationX(fabXY[0] - (ctaXY[0] / 2) + (fab.getWidth() / 2))
-//                        .translationY(fabXY[1] - (ctaXY[1] / 2) + (fab.getHeight() / 2))
-//                        .withEndAction(new Runnable() {
-//                            @Override
-//                            public void run() {
-//
-//                            }
-//                        });
-
-
-            }
-
-            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-            private void animateToFab(final FloatingActionButton fab, final View bottomFabBar) {
-//                final int[] fabXY = new int[2];
-//                fab.getLocationOnScreen(fabXY);
-//
-//                final int[] ctaXY = new int[2];
-//                cta.getLocationOnScreen(ctaXY);
-//
-//                final Animator circularReveal = ViewAnimationUtils.createCircularReveal(
-//                        cta,
-//                        cta.getWidth() / 2,
-//                        cta.getHeight() / 2,
-//                        cta.getWidth(),
-//                        fab.getWidth() / 2);
-//
-//                circularReveal.addListener(new AnimatorListenerAdapter() {
-//                    @Override
-//                    public void onAnimationEnd(Animator animation) {
-//                        cta.setVisibility(View.GONE);
-//                    }
-//                });
-//
-//                circularReveal.start();
-
-//                fab.animate()
-//                        .translationX(fabXY[0] - (ctaXY[0] / 2) + (fab.getWidth() / 2))
-//                        .translationY(fabXY[1] - (ctaXY[1] / 2) + (fab.getHeight() / 2))
-//                        .withEndAction(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                circularReveal.start();
-//                            }
-//                        });
-
-
-                }
-            }
-        });*/
     }
 
 
@@ -238,82 +88,6 @@ public class ProductDetailFragment extends Fragment {
         new DetailAsyncTask().executeOnExecutor(
                 AsyncTask.THREAD_POOL_EXECUTOR,
                 getArguments().getString(ARGUMENTS_PRODUCT_ID));
-    }
-
-    private Point center(View view) {
-        return new Point((int) view.getX() + (view.getWidth() / 2), (int) view.getY() + (view.getHeight() / 2));
-    }
-
-    private void testCloseReveal() {
-        mExtendedFab.setVisibility(View.VISIBLE);
-        mFab.setVisibility(View.GONE);
-
-        mFab.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-            public boolean mSet;
-
-            @Override
-            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                if (!mSet) {
-                    mSet = true;
-                    mFab.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            final Animator animator = closeWithReveal(mExtendedFab, mFab);
-                            animator.addListener(new AnimatorListenerAdapter() {
-                                @Override
-                                public void onAnimationEnd(Animator animation) {
-                                    animation.setupEndValues();
-                                }
-                            });
-                            animator.start();
-                        }
-                    }, 1000);
-                }
-            }
-        });
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private Animator closeWithReveal(View mExtendedFab, FloatingActionButton mFab) {
-        return ViewAnimationUtils.createCircularReveal(
-                mExtendedFab,
-                mExtendedFab.getWidth() / 2,
-                mExtendedFab.getHeight() / 2,
-                mExtendedFab.getWidth(),
-                mFab.getWidth() / 2);
-    }
-
-    private void testOpenReveal() {
-        mExtendedFab.setVisibility(View.VISIBLE);
-        mFab.setVisibility(View.GONE);
-
-        mFab.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-            public boolean mSet;
-
-            @Override
-            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                if (!mSet) {
-                    mSet = true;
-                    mFab.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            final Animator animator = openWithReveal(mExtendedFab, mFab);
-                            animator.start();
-                        }
-                    }, 1000);
-                }
-        }
-        });
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private Animator openWithReveal(View extendedFab, FloatingActionButton fab) {
-        return ViewAnimationUtils.createCircularReveal(
-                extendedFab,
-                extendedFab.getWidth() / 2,
-                extendedFab.getHeight() / 2,
-                fab.getWidth() / 2,
-                extendedFab.getWidth());
     }
 
     private class DetailAsyncTask extends AsyncTask<String, Void, Product> {
@@ -340,13 +114,13 @@ public class ProductDetailFragment extends Fragment {
     private void renderProduct(Product product) {
         mName.setText(product.getName());
         mPrice.setText(product.getPrice());
-        mModel.setText(String.format("Model\t %s", product.getModel()));
-        mManufacturer.setText(String.format("Manufacturer\t %s", product.getManufacturer()));
-        mClass.setText(String.format("Starship Class\t %s", product.getStarshipClass()));
-        mPassengers.setText(String.format("Passengers\t %s", product.getPassengers()));
-        mCrew.setText(String.format("Crew\t %s", product.getCrew()));
-        mCargo.setText(String.format("Cargo capacity\t %s", product.getCargoCapacity()));
-        mHyperdriveRating.setText(String.format("Hyperdrive rating\t %s", product.getHyperDriveRating()));
+        mModel.setText(product.getModel());
+        mManufacturer.setText(product.getManufacturer());
+        mClass.setText(product.getStarshipClass());
+        mPassengers.setText(product.getPassengers());
+        mCrew.setText(product.getCrew());
+        mCargo.setText(product.getCargoCapacity());
+        mHyperdriveRating.setText(product.getHyperDriveRating());
 
         Picasso.with(getActivity()).
                 load(product.getImage())
