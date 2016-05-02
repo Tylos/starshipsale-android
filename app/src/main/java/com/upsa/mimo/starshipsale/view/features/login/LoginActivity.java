144cac413e253fbbae27b712efb7a61b40a0e6aa
Tylos <grtylos@gmail.com>
2016-05-02 22:07:57 +0200

    Remove login fragment to simplify example

--- app/src/main/java/com/upsa/mimo/starshipsale/view/features/login/LoginActivity.java
+++ app/src/main/java/com/upsa/mimo/starshipsale/view/features/login/LoginActivity.java
@@ -2,14 +2,30 @@ package com.upsa.mimo.starshipsale.view.features.login;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ViewSwitcher;
 
+import com.upsa.mimo.starshipsale.BuildConfig;
 import com.upsa.mimo.starshipsale.R;
+import com.upsa.mimo.starshipsale.api.session.SessionRepository;
+import com.upsa.mimo.starshipsale.domain.entities.Session;
+import com.upsa.mimo.starshipsale.view.MainActivity;
+
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText passwordInput;
+    private EditText emailInput;
+    private ViewSwitcher viewSwitcher;
 
     public static void launch(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
@@ -19,7 +35,21 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
+        emailInput = (EditText) findViewById(R.id.email_input);
+        passwordInput = (EditText) findViewById(R.id.password_input);
+        viewSwitcher = (ViewSwitcher) findViewById(R.id.cta_view_switcher);
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new LoginAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,
+                        new Pair<>(
+                                emailInput.getText().toString(),
+                                passwordInput.getText().toString()
+                        ));
+            }
+        });
     }
 
     @Override
@@ -42,5 +72,40 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private class LoginAsyncTask extends AsyncTask<Pair<String, String>, Void, Session> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            viewSwitcher.showNext();
+        }
+
+        @Override
+        protected Session doInBackground(Pair<String, String>... params) {
+            try {
+                return new SessionRepository(LoginActivity.this, BuildConfig.SERVER_REST_URL)
+                        .login(
+                                params[0].first,
+                                params[0].second);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Session session) {
+            super.onPostExecute(session);
+            viewSwitcher.showPrevious();
+            if (session != null) {
+                MainActivity.launch(LoginActivity.this);
+                finish();
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), R.string.generic_error, Snackbar.LENGTH_SHORT).show();
+            }
+        }
     }
 }
