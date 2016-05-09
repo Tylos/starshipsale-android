ea81ea9070498bee570bbf39749659da6eb258a7
Tylos <grtylos@gmail.com>
2016-05-09 22:38:52 +0200

    Simplify login async task and prepare to use bundle

--- app/src/main/java/com/upsa/mimo/starshipsale/view/features/login/LoginActivity.java
+++ app/src/main/java/com/upsa/mimo/starshipsale/view/features/login/LoginActivity.java
@@ -6,9 +6,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ViewSwitcher;
@@ -17,7 +14,6 @@ import com.upsa.mimo.starshipsale.BuildConfig;
 import com.upsa.mimo.starshipsale.R;
 import com.upsa.mimo.starshipsale.api.session.SessionRepository;
 import com.upsa.mimo.starshipsale.domain.entities.Session;
-import com.upsa.mimo.starshipsale.view.MainActivity;
 
 import java.io.IOException;
 
@@ -37,45 +33,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+        configureView();
+    }
+
+    private void configureView() {
         emailInput = (EditText) findViewById(R.id.email_input);
         passwordInput = (EditText) findViewById(R.id.password_input);
         viewSwitcher = (ViewSwitcher) findViewById(R.id.cta_view_switcher);
+        //noinspection ConstantConditions
         findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new LoginAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,
-                        new Pair<>(
-                                emailInput.getText().toString(),
-                                passwordInput.getText().toString()
-                        ));
+                        emailInput.getText().toString(), passwordInput.getText().toString());
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    private class LoginAsyncTask extends AsyncTask<Pair<String, String>, Void, Session> {
+    private class LoginAsyncTask extends AsyncTask<String, Void, Bundle> {
 
         @Override
         protected void onPreExecute() {
@@ -84,12 +59,12 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         @Override
-        protected Session doInBackground(Pair<String, String>... params) {
+        protected Bundle doInBackground(String... params) {
             try {
-                return new SessionRepository(LoginActivity.this, BuildConfig.SERVER_REST_URL)
-                        .login(
-                                params[0].first,
-                                params[0].second);
+                final Session session = new SessionRepository(LoginActivity.this, BuildConfig.SERVER_REST_URL)
+                        .login(params[0], params[1]);
+
+                return new Bundle();
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
@@ -97,11 +72,10 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(Session session) {
-            super.onPostExecute(session);
+        protected void onPostExecute(Bundle result) {
+            super.onPostExecute(result);
             viewSwitcher.showPrevious();
-            if (session != null) {
-                MainActivity.launch(LoginActivity.this);
+            if (result != null) {
                 finish();
             } else {
                 Snackbar.make(findViewById(android.R.id.content), R.string.generic_error, Snackbar.LENGTH_SHORT).show();
