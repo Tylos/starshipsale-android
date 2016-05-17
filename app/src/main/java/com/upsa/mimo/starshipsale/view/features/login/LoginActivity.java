32fee258ba7c564ea7aeb6a5e61753f3cec4f915
Tylos <grtylos@gmail.com>
2016-05-18 00:32:24 +0200

    Refactor LoginActivity to take into account AccountAuthenticatorResponse

--- app/src/main/java/com/upsa/mimo/starshipsale/view/features/login/LoginActivity.java
+++ app/src/main/java/com/upsa/mimo/starshipsale/view/features/login/LoginActivity.java
@@ -1,5 +1,8 @@
 package com.upsa.mimo.starshipsale.view.features.login;
 
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -22,6 +25,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText emailInput;
     private ViewSwitcher viewSwitcher;
 
+    private AccountAuthenticatorResponse mAccountAuthenticatorResponse = null;
+    private Bundle mResultBundle = null;
+
     public static void launch(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
         context.startActivity(intent);
@@ -33,6 +39,35 @@ public class LoginActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
         configureView();
+        initAuthentication();
+    }
+
+    private void initAuthentication() {
+        mAccountAuthenticatorResponse =
+                getIntent().getParcelableExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE);
+
+        if (mAccountAuthenticatorResponse != null) {
+            mAccountAuthenticatorResponse.onRequestContinued();
+        }
+    }
+
+    public final void setAccountAuthenticatorResult(Bundle result) {
+        mResultBundle = result;
+    }
+
+    @Override
+    public void finish() {
+        if (mAccountAuthenticatorResponse != null) {
+            // send the result bundle back if set, otherwise send an error.
+            if (mResultBundle != null) {
+                mAccountAuthenticatorResponse.onResult(mResultBundle);
+            } else {
+                mAccountAuthenticatorResponse.onError(AccountManager.ERROR_CODE_CANCELED,
+                        "canceled");
+            }
+            mAccountAuthenticatorResponse = null;
+        }
+        super.finish();
     }
 
     private void configureView() {
@@ -59,13 +94,25 @@ public class LoginActivity extends AppCompatActivity {
 
         @Override
         protected Bundle doInBackground(String... params) {
+            final Bundle bundle = new Bundle();
             try {
-                new SessionRepository(LoginActivity.this, BuildConfig.SERVER_REST_URL).login(params[0], params[1]);
+                final String userName = params[0];
+                final String userPassword = params[1];
+                final String accountType = getString(R.string.account_type);
 
-                return new Bundle();
+                new SessionRepository(LoginActivity.this, BuildConfig.SERVER_REST_URL).register(userName, userPassword);
+
+                Account account = new Account(userName, accountType);
+                AccountManager accountManager = AccountManager.get(getApplicationContext());
+                accountManager.addAccountExplicitly(account, userPassword, null);
+
+                bundle.putString(AccountManager.KEY_ACCOUNT_NAME, userName);
+                bundle.putString(AccountManager.KEY_ACCOUNT_TYPE, accountType);
+                return bundle;
             } catch (IOException e) {
-                e.printStackTrace();
-                return null;
+                bundle.putInt(AccountManager.KEY_ERROR_CODE, AccountManager.ERROR_CODE_NETWORK_ERROR);
+                bundle.putString(AccountManager.KEY_ERROR_MESSAGE, "Something went wrong " + e.getMessage());
+                return bundle;
             }
         }
 
@@ -73,10 +120,14 @@ public class LoginActivity extends AppCompatActivity {
         protected void onPostExecute(Bundle result) {
             super.onPostExecute(result);
             viewSwitcher.showPrevious();
-            if (result != null) {
+            if (result.containsKey(AccountManager.KEY_ACCOUNT_NAME)) {
+                setAccountAuthenticatorResult(result);
+                setResult(RESULT_OK);
                 finish();
             } else {
-                Snackbar.make(findViewById(android.R.id.content), R.string.generic_error, Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(findViewById(android.R.id.content),
+                        result.getString(AccountManager.KEY_ERROR_MESSAGE),
+                        Snackbar.LENGTH_SHORT).show();
             }
         }
     }
