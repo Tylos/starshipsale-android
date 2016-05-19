b757b2157ec6d34e96869dd9558bd06eb6b37f55
Tylos <grtylos@gmail.com>
2016-05-19 02:35:20 +0200

    Add support for updateCredentials in LoginActivity

--- app/src/main/java/com/upsa/mimo/starshipsale/view/features/login/LoginActivity.java
+++ app/src/main/java/com/upsa/mimo/starshipsale/view/features/login/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.upsa.mimo.starshipsale.view.features.login;
 
+import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
 import android.accounts.AccountManager;
 import android.accounts.AccountManagerCallback;
@@ -28,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private AccountAuthenticatorResponse mAccountAuthenticatorResponse = null;
     private Bundle mResultBundle = null;
+    private Account account = null;
 
     public static void launch(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
@@ -39,8 +41,20 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+        final Bundle extras = getIntent().getExtras();
+        if (containsAccount(extras)) {
+            account = new Account(
+                    extras.getString(AccountManager.KEY_ACCOUNT_NAME),
+                    extras.getString(AccountManager.KEY_ACCOUNT_TYPE)
+            );
+        }
         configureView();
         initAuthentication();
+    }
+
+    private boolean containsAccount(Bundle extras) {
+        return extras.containsKey(AccountManager.KEY_ACCOUNT_NAME) &&
+                extras.containsKey(AccountManager.KEY_ACCOUNT_TYPE);
     }
 
     private void initAuthentication() {
@@ -73,6 +87,11 @@ public class LoginActivity extends AppCompatActivity {
 
     private void configureView() {
         emailInput = (EditText) findViewById(R.id.email_input);
+        if (!isNewAccount()) {
+            emailInput.setText(account.name);
+            emailInput.setEnabled(false);
+            passwordInput.requestFocus();
+        }
         passwordInput = (EditText) findViewById(R.id.password_input);
         viewSwitcher = (ViewSwitcher) findViewById(R.id.cta_view_switcher);
         //noinspection ConstantConditions
@@ -80,47 +99,97 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AccountManager accountManager = AccountManager.get(getApplicationContext());
-
-                String accountType = getString(R.string.account_type);
-                Bundle accountOptions = new Bundle();
-                accountOptions.putString(Authenticator.ADD_ACCOUNT_USER_NAME, emailInput.getText().toString());
-                accountOptions.putString(Authenticator.ADD_ACCOUNT_USER_PASSWORD, passwordInput.getText().toString());
-
-                viewSwitcher.showNext();
-                accountManager.addAccount(
-                        accountType,
-                        null,
-                        null,
-                        accountOptions,
-                        null,
-                        new AccountManagerCallback<Bundle>() {
-                            @Override
-                            public void run(AccountManagerFuture<Bundle> future) {
-                                viewSwitcher.showPrevious();
-
-                                try {
-                                    final Bundle result = future.getResult();
-                                    if (result.containsKey(AccountManager.KEY_ACCOUNT_NAME)) {
-                                        setAccountAuthenticatorResult(result);
-                                        setResult(RESULT_OK);
-                                        finish();
-                                    } else {
-                                        Snackbar.make(findViewById(android.R.id.content),
-                                                result.getString(AccountManager.KEY_ERROR_MESSAGE),
-                                                Snackbar.LENGTH_SHORT).show();
-                                    }
-                                } catch (OperationCanceledException e) {
-                                    e.printStackTrace();
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                } catch (AuthenticatorException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        },
-                        null
-                );
+                if (isNewAccount()) {
+                    addNewAccount(accountManager);
+                } else {
+                    confirmCredentials(accountManager);
+                }
             }
         });
+    }
+
+    private void addNewAccount(AccountManager accountManager) {
+        String accountType = getString(R.string.account_type);
+        Bundle accountOptions = new Bundle();
+        accountOptions.putString(Authenticator.ADD_ACCOUNT_USER_NAME, emailInput.getText().toString());
+        accountOptions.putString(Authenticator.ADD_ACCOUNT_USER_PASSWORD, passwordInput.getText().toString());
+
+        viewSwitcher.showNext();
+        accountManager.addAccount(
+                accountType,
+                null,
+                null,
+                accountOptions,
+                null,
+                new AccountManagerCallback<Bundle>() {
+                    @Override
+                    public void run(AccountManagerFuture<Bundle> future) {
+                        viewSwitcher.showPrevious();
+
+                        try {
+                            final Bundle result = future.getResult();
+                            if (result.containsKey(AccountManager.KEY_ACCOUNT_NAME)) {
+                                setAccountAuthenticatorResult(result);
+                                setResult(RESULT_OK);
+                                finish();
+                            } else {
+                                Snackbar.make(findViewById(android.R.id.content),
+                                        result.getString(AccountManager.KEY_ERROR_MESSAGE),
+                                        Snackbar.LENGTH_SHORT).show();
+                            }
+                        } catch (OperationCanceledException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (AuthenticatorException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                null
+        );
+    }
+
+    private void confirmCredentials(AccountManager accountManager) {
+        Bundle options = new Bundle();
+        options.putString(Authenticator.UPDATE_CREDENTIALS_USER_PASSWORD, passwordInput.getText().toString());
+
+        viewSwitcher.showNext();
+        accountManager.updateCredentials(
+                account,
+                getString(R.string.authtoken_type),
+                options,
+                null,
+                new AccountManagerCallback<Bundle>() {
+                    @Override
+                    public void run(AccountManagerFuture<Bundle> future) {
+                        viewSwitcher.showPrevious();
+
+                        try {
+                            final Bundle result = future.getResult();
+                            if (result.containsKey(AccountManager.KEY_ACCOUNT_NAME)) {
+                                setAccountAuthenticatorResult(result);
+                                setResult(RESULT_OK);
+                                finish();
+                            } else {
+                                Snackbar.make(findViewById(android.R.id.content),
+                                        result.getString(AccountManager.KEY_ERROR_MESSAGE),
+                                        Snackbar.LENGTH_SHORT).show();
+                            }
+                        } catch (OperationCanceledException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (AuthenticatorException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                null
+        );
+    }
+
+    public boolean isNewAccount() {
+        return account == null;
     }
 }
